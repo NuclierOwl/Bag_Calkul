14da1cc479b38423a6a78e066fa916becb86c85e
Userok <dullahannomer3@gmail.com>
2025-04-24 09:47:14 +0300

    Pravki

--- TestProject1/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1458f6db61173ac3aa110427aac52989087265")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6719d5323f0ab610dd222adacd8ed6cd7fce2efe")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
